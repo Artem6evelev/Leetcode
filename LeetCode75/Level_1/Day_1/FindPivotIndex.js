ff37cfe539a77c96d35ff241048b2c5b06ea68f1
Artem6evelev <artem6evelev92@gmail.com>
2022-12-25 20:39:53 +0200

    pivotIndex leetcode task

--- LeetCode75/Level_1/Day_1/FindPivotIndex.js
+++ LeetCode75/Level_1/Day_1/FindPivotIndex.js
@@ -38,4 +38,17 @@
 
 let nums = [1, 7, 3, 6, 5, 6];
 
-let pivotIndex = (nums) => {};
+let pivotIndex = (nums) => {
+  let totalSum = 0,
+    leftSum = 0;
+  nums.forEach((e) => {
+    totalSum += e;
+  });
+  for (let i = 0; i < nums.length; i++) {
+    if (totalSum - leftSum - nums[i] === leftSum) {
+      return i;
+    }
+    leftSum += nums[i];
+  }
+  return -1;
+};
