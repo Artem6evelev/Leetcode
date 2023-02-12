704b5820445c9f799ad226ae16966476b9bfb8dd
Artem6evelev <artem6evelev92@gmail.com>
2023-02-12 15:47:02 +0200

    remove duplicates from sorted array

---
+++ Leetcode/1_Remove_Duplicates_from_Sorted_Array/index.js
@@ -0,0 +1,64 @@
+/*
+Given an integer array nums sorted in non-decreasing order, 
+remove the duplicates in-place such that each unique element appears only once.
+The relative order of the elements should be kept the same.
+Since it is impossible to change the length of the array in some languages,
+you must instead have the result be placed in the first part of the array nums. 
+More formally, if there are k elements after removing the duplicates, 
+then the first k elements of nums should hold the final result. 
+It does not matter what you leave beyond the first k elements.
+Return k after placing the final result in the first k slots of nums.
+Do not allocate extra space for another array. 
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+//* Example 1:
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+//* Example 2:
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Constraints:
+1 <= nums.length <= 3 * 104
+-100 <= nums[i] <= 100
+nums is sorted in non-decreasing order.
+*/
+
+let array = [1, 2, 3, 1, 1, 4, 5, 5, 5, 6, 7];
+
+var removeDuplicates = function (arr) {
+  let indexCounter = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i - 1]) {
+      arr[indexCounter] = arr[i];
+      indexCounter++;
+    }
+  }
+  return indexCounter;
+};
+
+/* 
+//* Explanation:
+Function that removes duplicates from an array. 
+The function takes an array as its argument and returns the length of the new, 
+deduplicated array.
+The function uses two variables: indexCounter and i. 
+indexCounter is initialized to zero and is used to keep track of 
+the position of the next unique element in the array. 
+i is used as a loop counter to iterate over all elements in the array.
+The loop starts with i = 0 and continues as long as i is 
+less than the length of the array. For each iteration of the loop, 
+the function checks if the current element (arr[i]) is equal to the previous 
+element (arr[i - 1]). If they are not equal, the function copies the 
+current element to the position indexCounter in the array and increments 
+indexCounter by 1.
+When the loop finishes, the function returns the final value of indexCounter, 
+which is the length of the deduplicated array.
+*/
+
+console.log("removeDuplicates", removeDuplicates(array));
